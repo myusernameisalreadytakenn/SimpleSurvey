47182a3ad8be8c3afa516ba58cbbc727a40b3d5c
Rustamov Arsen <43421600+myusernameisalreadytakenn@users.noreply.github.com>
2020-12-15 20:47:37 +0200

    Update survay.js

--- src/js/models/survay.js
+++ src/js/models/survay.js
@@ -1,1 +1,23 @@
+const connection = require("../connection");
+const Sequelize = require("sequelize");
 
+
+const Model = Sequelize.Model;
+class Role extends Model {}
+Role.init({
+
+    RoleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    Role: {
+        type: Sequelize.STRING(45)
+    }
+}, {
+    sequelize: connection,
+    modelName: 'survay'
+
+});
+
+module.exports = Role;
