315c64981ac2a609451bd29fd387ac8facc9f642
Rustamov Arsen <43421600+myusernameisalreadytakenn@users.noreply.github.com>
2020-12-15 20:45:28 +0200

    Update user.js

--- src/js/models/user.js
+++ src/js/models/user.js
@@ -1,36 +1,38 @@
-const connection = require('../connection');
-const Sequilize = require('sequelize');
+const connection = require("../connection");
+const Sequelize = require("sequelize");
 
-class User extends Sequilize.Model {}
 
-User.init(
-    {
-        id: {
-            type: Sequilize.INTEGER,
-            primaryKey: true
-        },
-        name: {
-            type: Sequilize.STRING(300),
-            allowNull: false
-        },
-        password: {
-            type: Sequilize.STRING(300),
-            allowNull: false
-        },
-        email: {
-            type: Sequilize.STRING(200),
-            allowNull: false
-        },
-        status: {
-            type: Sequilize.STRING(100),
-            allowNull: true
-        }
+const Model = Sequelize.Model;
+class User extends Model {}
+
+User.init({
+    // attributes
+    UserId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
     },
-    {
-        sequelize: connection,
-        freezeTableName: true,
-        modelName: 'User'
+    username: {
+        type: Sequelize.STRING(45),
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING(32),
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+    },
+    avatar: {
+        type: Sequelize.STRING(500)
     }
-);
+}, {
+    sequelize: connection,
+    modelName: 'User',
+    tableName: "User",
+    freezeTableName: true
+    // options
+});
 
-module.exports = User
+module.exports = User;
