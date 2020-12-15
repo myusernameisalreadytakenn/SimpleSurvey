1fd89dffac555e0986891d5819a87f03c724bcc2
Rustamov Arsen <43421600+myusernameisalreadytakenn@users.noreply.github.com>
2020-12-15 20:26:53 +0200

    Update connection.js

--- src/js/connection.js
+++ src/js/connection.js
@@ -1,3 +1,19 @@
- const Sequilize = require('sequelize');
+const Sequelize = require('sequelize');
 
-module.exports = new Sequilize(require('../../config/db'))
+const options = {
+    database: 'test',
+    username: 'admin',
+    password: 'n03rn7tp',
+    dialect: 'mysql',
+    define: { timestamps: false }, 
+    
+    pool: {
+        max: 5,
+        idle: 30000,
+        acquire: 60000,
+    },
+};
+
+const sequelize = new Sequelize(options);
+
+module.exports = sequelize;
